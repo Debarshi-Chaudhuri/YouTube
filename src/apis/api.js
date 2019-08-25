05cb466ac0ab552c37077969f90bf9b985fb4e94
Debarshi-Chaudhuri <dchaudhuri17@gmail.com>
2019-08-25 22:40:02 +0530

    first commit

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,5 +1,5 @@
 export const fetchSearchData=(data)=>{
-    return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${data}&maxResults=15&type=video&key=AIzaSyBjhFK9MovixovHfXxDM3RUHST7ClMAFNY`);
+    return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${data}&safeSearch=strict&maxResults=15&type=video&key=AIzaSyBjhFK9MovixovHfXxDM3RUHST7ClMAFNY`);
 }
 export const fetchHomeData=()=>{
     return fetch('https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=15&key=AIzaSyBjhFK9MovixovHfXxDM3RUHST7ClMAFNY');
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -3,9 +3,9 @@ import '../App.css'
 import icon from '../icon.svg'
 import { Button } from "@material-ui/core";
 export const Searchbar=(props)=>{
-    const [text,setState]=useState('');
+    const [text,setText]=useState('');
     const change=(event)=>{
-        setState({text:event.target.value})
+        setText(event.target.value)
     }
     
     return(
@@ -14,7 +14,7 @@ export const Searchbar=(props)=>{
             <input type='text' style={{borderRadius:'5px',borderWidth:'1px',textIndent:'8px'}}  onChange={change} placeholder='Search' value={text} />
             <Button  variant='contained' style={{backgroundColor:'rgba(233, 233, 233, 0.966)',marginLeft:'4px',color:'white'}} onClick={()=>{
                 props.search(text);
-                setState({text:''})
+                setText('')
             }}><img src={icon} style={{color:'white'}} /></Button>
         </div>
     )
